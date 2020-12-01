4c4b00150d0cf7cd891eb47ad99a6e9fbb12d736
namhla1221 <namhlamthi21@gmail.com>
2020-12-01 10:38:55 +0200

    created some routes

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const flash = require('express-flash');
 const session = require('express-session');
+const Cart = require('./routes/cart');
+const Shopping_shoes = require('./routes/shopping_shoes');
+const CartService = require('./services/shopping_cart');
+const ShoppingService = require('./services/shoes');
 const app = express();
 
 let PORT = process.env.PORT || 3010;
@@ -32,6 +36,7 @@ const pool = new Pool({
 
 
 
+
 app.use(bodyParser.urlencoded({ extended: false}));
 
 app.use(bodyParser.json());
@@ -41,16 +46,128 @@ app.listen(PORT, function(){
     console.log('started on:', this.address().port);
 })
 
-app.get("/");
+app.get('/api/shoes', async function  (req, res) {
+    try {
+        let shoes = await shoppingServie.allShoes();
+        res.json({
+            status: "success",
+            data: shoes
+        })
+    } catch (e) {
+        res.json({
+           
+        })
+    }
+})
 
-app.get("/api/shoes");
 
-app.get("/api/shoes/brand/:brandname");
+app.post('/api/shoes',  async function(req, res)  {
+    try {
+        const {
+            brand,
+            color,
+            shoeSize,
+            quantity,
+            price
+        } = req.body;
+        if (brand !== undefined && shoeSize !== undefined && quantity !== undefined &&
+            price !== undefined && color !== undefined) {
+            let addNewShoe = await shoppingServie.addShoe(brand, color, shoeSize, price, quantity);
+            res.json({
+                status: "success",
+                data: addNewShoe
+            })
+        } else {
+            res.json({
+                
+            })
+        }
+    } catch (e) {
+        res.json({
+           
+        })
+    }
+})
 
-app.get("/api/shoes/size/:size");
 
-app.get("/api/shoes/brand/:brandname/size/:size");
+app.get('/api/shoes/brand/:brandname', async function (req, res)  {
+    try {
+        const {
+            brandname
+        } = req.params;
+        if (brandname !== '' && brandname !== undefined) {
+            let searchByBrand = await shoppingServie.findByBrand(brandname);
+            res.json({
+                status: "success",
+                data: searchByBrand
+            })
+        } else {
+            return false;
+        }
+    } catch (e) {
+        res.json({
+           
 
-app.post("/api/shoes/sold/:id");
+        })
+    }
+})
 
-app.post("/api/shoes");
+
+app.get('/api/shoes/size/:size', async function (req, res) {
+    try {
+        const {
+            size
+        } = req.params;
+        if (size !== '' && size !== undefined) {
+            let searchBySize = await shoppingServie.findBySize(size);
+            res.json({
+                status: "success",
+                data: searchBySize
+            })
+        } else {
+            return false;
+        }
+    } catch (e) {
+        res.json({
+          
+
+        })
+    }
+})
+
+app.get('/api/shoes/brand/:brandname/size/:size', async function (req, res) {
+    try {
+        const {
+            brandname,
+            size
+        } = req.params;
+        if (brandname !== '' && brandname !== undefined &&
+            size !== '' || size !== undefined) {
+            let searchByBrandAndSize =
+                await shoppingServie.findBybrandAndSize(brandname, size);
+            res.json({
+                status: "success",
+                data: searchByBrandAndSize
+            })
+        } else {
+            return false;
+        }
+    } catch (e) {
+        res.json({
+            
+
+        })
+    }
+})
+
+app.get('/api/delete/shoes');
+
+
+
+
+
+
+
+
+
+
