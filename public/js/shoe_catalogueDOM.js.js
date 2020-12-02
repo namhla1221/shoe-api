7a42195c97b385f880ef3e76553a65cee8e9f665
namhla1221 <namhlamthi21@gmail.com>
2020-12-02 11:24:14 +0200

    created dom elements

---
+++ public/js/shoe_catalogueDOM.js.js
@@ -0,0 +1,5 @@
+var brandElem = document.querySelector(".shoeBrand");
+var sizeElem = document.querySelector(".shoeSize");
+var colourElem = document.querySelector(".shoeColour");
+var shpeQtyElem = document.querySelector(".shoeQty");
+var brandElem = document.querySelector(".shoeBrand")
